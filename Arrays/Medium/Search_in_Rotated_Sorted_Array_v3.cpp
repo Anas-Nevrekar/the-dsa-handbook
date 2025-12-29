ef2ee1cd6232cf44c8bbd0182fe799128a3cc302
Anas Nevrekar <112456172+Anas-Nevrekar@users.noreply.github.com>
2025-12-29 14:51:43 +0530

    Add solution Search_in_Rotated_Sorted_Array_v3.cpp

---
+++ Arrays/Medium/Search_in_Rotated_Sorted_Array_v3.cpp
@@ -0,0 +1,27 @@
+// 33. Search_in_Rotated_Sorted_Array
+// Difficulty: Medium
+
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+        int lp = 0, rp = height.size() - 1;
+        long long maxWater = 0;
+
+        while (lp < rp) {
+            int width = rp - lp;
+            int ht = min(height[lp], height[rp]);
+            long long currWater = (long long) width * ht;
+            maxWater = max(maxWater, currWater);
+
+            if (height[lp] < height[rp]) {
+                lp++;
+            } else {
+                rp--;
+            }
+        }
+        return (int) maxWater;
+    }
+};
