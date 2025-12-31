72acc5901b0acf9fb71a67545f5b17521bfd6e43
Anas Nevrekar <112456172+Anas-Nevrekar@users.noreply.github.com>
2025-12-31 23:09:28 +0530

    Add solution Container_With_Most_Water_v1.c

---
+++ Arrays/Medium/Container_With_Most_Water_v1.c
@@ -0,0 +1,25 @@
+// 11. Container_With_Most_Water
+// Difficulty: Medium
+
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+          int lp = 0, rp = height.size() - 1;
+        long long maxWater = 0; // Use long long to prevent overflow
+
+        while (lp < rp) {
+            int width = rp - lp;
+            int ht = min(height[lp], height[rp]);
+            long long currWater = (long long)width * ht; // Cast to long long
+            maxWater = max(maxWater, currWater);
+
+            if (height[lp] < height[rp]) {
+                lp++;
+            } else {
+                rp--;
+            }
+        }
+
+        return (int)maxWater;
+    }
+};
